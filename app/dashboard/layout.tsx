7346c6cfec5b16a9e6e9c3ebd800634c2b217df2
Odaine Codner <odaine.codner@gmail.com>
2026-07-26 19:04:23 +0100

    fix: improve mobile scrolling behaviour

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -28,7 +28,7 @@ export default function DashboardLayout({
           {/* Scrollable page content */}
           {/* pb-20: bottom padding on mobile to avoid content hiding behind tab bar */}
           {/* lg:pb-0: remove that padding on desktop since no tab bar */}
-          <div className="flex-1 overflow-y-auto p-4 sm:p-6 pb-20 sm:pb-6 overscroll-none">
+          <div className="flex-1 min-h-0 overflow-y-auto p-4 sm:p-6 pb-20 sm:pb-6">
             <PageTransition>{children}</PageTransition>
           </div>
         </main>
